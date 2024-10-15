2451529f7abc4311f0acea350088585ed6748050
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-10-15 14:26:45 +0500

    ✨ Create input form

--- client/src/layout/Nav/Nav.jsx
+++ client/src/layout/Nav/Nav.jsx
@@ -1,13 +1,15 @@
 import styles from './Nav.module.scss';
 import NavButton from './NavButton.jsx';
+import NavForm from './NavForm.jsx';
 
 const Nav = () => {
   return (
     <div className={styles.nav}>
       <div className={styles.logo}>Spotify</div>
 
-      <div>
+      <div className={styles.nav__center}>
         <NavButton />
+        <NavForm />
       </div>
       <div>profile</div>
     </div>
---
+++ client/src/layout/Nav/NavForm.jsx
@@ -0,0 +1,17 @@
+import styles from './NavForm.module.scss';
+import { RiSearchLine } from 'react-icons/ri';
+
+const NavForm = () => {
+  return (
+    <div className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="What do you want to play?"
+      />
+      <RiSearchLine />
+    </div>
+  );
+};
+
+export default NavForm;
