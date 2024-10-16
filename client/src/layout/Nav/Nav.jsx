fcc542f8d5a5926882bd4c51395fcd774bb3cc8a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-10-16 17:01:26 +0500

    ♻️ Refactor Nav component using NavTooltip

--- client/src/layout/Nav/Nav.jsx
+++ client/src/layout/Nav/Nav.jsx
@@ -4,49 +4,45 @@ import NavForm from './NavForm.jsx';
 import Button from '../../components/Button/Button.jsx';
 import { RiHome4Line, RiNotification3Line } from 'react-icons/ri';
 import Avatar, { AvatarFallback } from '../../components/Avatar/Avatar.jsx';
-import Tooltip from '../../components/Tooltip/Tooltip.jsx';
+import NavTooltip from './NavTooltip.jsx';
 
-// TODO: Needs better refactoring for readability, maybe folder for component references? Or hook maybe?
 const Nav = () => {
   return (
     <div className={styles.nav}>
       <div className={styles.logo}>Spotify</div>
 
       <div className={styles.nav__center}>
-        <Tooltip>
-          <Tooltip.Trigger>
-            <NavButton icon={<RiHome4Line />} />
-          </Tooltip.Trigger>
-          <Tooltip.Content position="bottom">Home</Tooltip.Content>
-        </Tooltip>
-
+        <NavTooltip
+          trigger={<NavButton icon={<RiHome4Line />} />}
+          position="bottom"
+          content="Menu"
+        />
         <NavForm />
       </div>
 
       <div className="flex items-center gap-4">
         <Button>Login</Button>
 
-        <Tooltip>
-          <Tooltip.Trigger>
+        <NavTooltip
+          trigger={
             <NavButton
               isTransparent={true}
               isSmall={true}
               icon={<RiNotification3Line />}
             />
-          </Tooltip.Trigger>
-          <Tooltip.Content position="bottom">Whats New</Tooltip.Content>
-        </Tooltip>
-
-        <Tooltip>
-          <Tooltip.Trigger>
+          }
+          position="bottom"
+          content="Whats New"
+        />
+        <NavTooltip
+          trigger={
             <Avatar>
               <AvatarFallback>M</AvatarFallback>
             </Avatar>
-          </Tooltip.Trigger>
-          <Tooltip.Content position="bottom-left">
-            Maqsud Tolipov
-          </Tooltip.Content>
-        </Tooltip>
+          }
+          position="bottom-left"
+          content="Maqsud Tolipov"
+        />
       </div>
     </div>
   );
