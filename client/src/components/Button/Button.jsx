a7510acafbc38567db6090f8c52fdced4562c7bd
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-10-16 16:48:38 +0500

    ♻️ Refactor code

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,11 +1,7 @@
 import styles from './Button.module.scss';
 
-const Button = ({children}) => {
-  return (
-    <button className={styles.button}>
-      {children}
-    </button>
-  );
+const Button = ({ children }) => {
+  return <button className={styles.button}>{children}</button>;
 };
 
 export default Button;
