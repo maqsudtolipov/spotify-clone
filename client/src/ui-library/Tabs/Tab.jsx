a720301fd5c0ae9fa953e654f716000b079628a3
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-02 16:46:23 +0500

    🎨 Style tab component

--- client/src/ui-library/Tabs/Tab.jsx
+++ client/src/ui-library/Tabs/Tab.jsx
@@ -1,8 +1,14 @@
+import styles from './Tabs.module.scss';
+
 const Tab = ({ value, children, ...rest }) => {
   if (!value)
     return alert('🚨 Please provide value for Tab component to work properly.');
 
-  return <button {...rest}>{children}</button>;
+  return (
+    <button className={styles.tab} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Tab;
