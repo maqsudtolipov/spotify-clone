81564fa47819ae2520040334bbf8391799c9dd3f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-02 16:48:32 +0500

    🎨 Style TabsList component

--- client/src/ui-library/Tabs/TabsList.jsx
+++ client/src/ui-library/Tabs/TabsList.jsx
@@ -1,5 +1,11 @@
+import styles from './Tabs.module.scss';
+
 const TabsList = ({ children, ...rest }) => {
-  return <div {...rest}>{children}</div>;
+  return (
+    <div className={styles.tabsList} {...rest}>
+      {children}
+    </div>
+  );
 };
 
 export default TabsList;
