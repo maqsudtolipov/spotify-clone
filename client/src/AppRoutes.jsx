08626a1fc60660e4b8b7adde941568d2bffdb5d2
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-04 14:59:00 +0500

    ⚙️ Remove boilerplate code

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -16,41 +16,7 @@ const AppRoutes = () => {
             <div className={styles.app}>
               <Nav />
               <Library />
-              <main className="bg-gradient-to-b from-sky-500/[0.5] to-neutral-900 rounded-lg p-4 overflow-hidden">
-                <div className="mb-2">
-                  <Tabs defaultValue="all">
-                    <TabsList>
-                      <Tab value="all">All</Tab>
-                      <Tab value="artists">Artists</Tab>
-                      <Tab value="playlsits">Playlists</Tab>
-                      <Tab value="songs">Songs</Tab>
-                    </TabsList>
-                  </Tabs>
-                </div>
-                <Tabs defaultValue="one">
-                  <TabsList>
-                    <Tab value="one">One</Tab>
-                    <Tab value="two">Two</Tab>
-                    <Tab value="three">Three</Tab>
-                    <Tab value="four">Four</Tab>
-                    <Tab value="five">Five</Tab>
-                    <Tab value="six">Six</Tab>
-                    <Tab value="seven">Seven</Tab>
-                    <Tab value="eight">Eight</Tab>
-                    <Tab value="nine">Nine</Tab>
-                    <Tab value="ten">Ten</Tab>
-                    <Tab value="eleven">Eleven</Tab>
-                    <Tab value="twelve">Twelve</Tab>
-                    <Tab value="thirteen">Thirteen</Tab>
-                    <Tab value="sixteen">Sixteen</Tab>
-                    <Tab value="seventeen">Seventeen</Tab>
-                    <Tab value="eighteen">Eighteen</Tab>
-                    <Tab value="nineteen">Nineteen</Tab>
-                    <Tab value="tenteen">Tenteen</Tab>
-                    <Tab value="eleventeen">Eleventeen</Tab>
-                  </TabsList>
-                </Tabs>
-              </main>
+              <main className="bg-gradient-to-b from-sky-500/[0.5] to-neutral-900 rounded-lg p-4 overflow-hidden"></main>
               <div className="p-4">Player</div>
             </div>
           }
