83c8398be6b16ba7965e4cda5285b2bff9f078fa
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-04 11:56:53 +0500

    ✨ Show scroll buttons, based on scroll position

--- client/src/ui-library/Tabs/TabsList.jsx
+++ client/src/ui-library/Tabs/TabsList.jsx
@@ -1,11 +1,51 @@
 import styles from './Tabs.module.scss';
 import ArrowButton from './ArrowButton.jsx';
+import { useEffect, useRef, useState } from 'react';
 
 const TabsList = ({ children, ...rest }) => {
+  const [showRightArrow, setshowRightArrow] = useState();
+  const [showLeftArrow, setShowLeftArrow] = useState();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const el = ref.current;
+
+    const eventListener = () => {
+      const differenceRight =
+        el.scrollWidth - el.getBoundingClientRect().width - el.scrollLeft;
+      console.log(
+        el.scrollWidth,
+        el.getBoundingClientRect().width,
+        el.scrollLeft,
+      );
+
+      if (differenceRight >= 10) {
+        setshowRightArrow(true);
+      } else {
+        setshowRightArrow(false);
+      }
+
+      if (el.scrollLeft >= 10) {
+        setShowLeftArrow(true);
+      } else {
+        setShowLeftArrow(false);
+      }
+    };
+
+    el.addEventListener('scroll', eventListener);
+    return () => {
+      el.removeEventListener('scroll', eventListener);
+    };
+  }, []);
+
   return (
     <div className="relative" {...rest}>
-      <div className={styles.tabsList}>{children}</div>
-      <ArrowButton />
+      <div className={styles.tabsList} ref={ref}>
+        {children}
+      </div>
+
+      {showLeftArrow && <ArrowButton position="left" />}
+      {showRightArrow && <ArrowButton position="right" />}
     </div>
   );
 };
