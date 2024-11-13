499a7ee8984f5e39144b06f011fb1a6e71179957
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-14 04:26:50 +0500

    🧪 Test Vitest library

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+/// reference types=”vitest” />
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    root: 'src/',
+  },
 });
