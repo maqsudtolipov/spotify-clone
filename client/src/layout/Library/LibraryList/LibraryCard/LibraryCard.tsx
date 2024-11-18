3ca702c223368430bccbea9663c52bf6e0fc327b
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-18 14:35:06 +0500

    🎨 Add styles for playing Card

--- client/src/layout/Library/LibraryList/LibraryCard/LibraryCard.tsx
+++ client/src/layout/Library/LibraryList/LibraryCard/LibraryCard.tsx
@@ -1,6 +1,7 @@
 import styles from './LibraryCard.module.scss';
 import CardImage from './CardImage.tsx';
 import CardInfo from './CardInfo.tsx';
+import { RiVolumeDownFill } from 'react-icons/ri';
 
 interface LibraryCardData {
   img: string;
@@ -12,12 +13,17 @@ interface LibraryCardProps {
 }
 
 const LibraryCard = ({ data }: LibraryCardProps) => {
+  const isPlaying = false;
+
   const { img, name } = data;
 
   return (
-    <li className={styles.libraryCard}>
+    <li
+      className={`${styles.libraryCard} ${isPlaying ? styles.libraryCardPlaying : ''}`}
+    >
       <CardImage src={img} name={`Cover for ${name}`} />
       <CardInfo name={name} type={'playlist'} />
+      {isPlaying && <RiVolumeDownFill className={styles.cardIcon} />}
     </li>
   );
 };
