eaf33e30cd8246fa706d34240dd0349e5ab39c57
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-18 14:35:06 +0500

    🎨 Add styles for playing Card

--- client/src/layout/Library/LibraryList/LibraryCard/CardInfo.tsx
+++ client/src/layout/Library/LibraryList/LibraryCard/CardInfo.tsx
@@ -8,17 +8,14 @@ interface CardInfoProps {
   owner?: string;
 }
 
-// TODO: style Info descriptions
-// TODO: add playing styles
-
 const combineDescription = (arr: string[]): string => {
   return arr.join(' • ');
 };
 
 const CardInfo = ({ name, isPinned, type, length, owner }: CardInfoProps) => {
   return (
-    <div>
-      <span>{name}</span>
+    <div className={styles.cardInfo}>
+      <span className={styles.cardInfoName}>{name}</span>
       <div className={styles.cardInfoDescription}>
         <span>{combineDescription(['Playlist', 'Christina', '12 songs'])}</span>
       </div>
