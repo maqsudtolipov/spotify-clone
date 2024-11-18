ef3edc3569add3de810ea2f7a72d50d5f903d36f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-18 15:52:11 +0500

    ✨ Sort items by pinned and created date by default

--- client/src/layout/Library/LibraryList/LibraryList.jsx
+++ client/src/layout/Library/LibraryList/LibraryList.jsx
@@ -38,10 +38,27 @@ const LibraryList = () => {
       const type = faker.datatype.boolean() ? 'artist' : 'playlist';
       const isPinned = faker.number.int(20) <= 1;
 
-      arr.push({ img, name, isPinned, type });
+      // Issues timezone difference
+      const createdAt = faker.date
+        .between({
+          from: '2020-01-01',
+          to: Date.now(),
+        })
+        .toUTCString()
+        .toLocaleString();
+
+      arr.push({ img, name, isPinned, type, createdAt });
     }
 
-    arr.sort((a, b) => b.isPinned - a.isPinned);
+    // Sort by recently added
+    // arr.sort(
+    //   (a, b) =>
+    //     b.isPinned - a.isPinned ||
+    //     new Date(b.createdAt) - new Date(a.createdAt),
+    // );
+
+    // Sort by alphabetical
+    // arr.sort((a, b) => b.isPinned - a.isPinned || a.name.localeCompare(b.name));
 
     setFakeList(arr);
   }, []);
