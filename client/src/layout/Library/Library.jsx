b43469a80caadb3d7ac6aabcc070fd0dc077fbc6
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-21 10:45:48 +0500

    🦆 Migrate library sort from hooks to redux

--- client/src/layout/Library/Library.jsx
+++ client/src/layout/Library/Library.jsx
@@ -3,7 +3,6 @@ import LibraryHeader from './LibraryHeader/LibraryHeader.jsx';
 import LibraryList from './LibraryList/LibraryList.jsx';
 import { useState } from 'react';
 import LibrarySearch from './LibrarySearch/LibrarySearch.tsx';
-import { LibrarySortProvider } from '../../hooks/useLibrarySort.tsx';
 
 const Library = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -13,15 +12,13 @@ const Library = () => {
   };
 
   return (
-    <LibrarySortProvider>
-      <div
-        className={`${styles.library} ${isCollapsed ? styles.libraryCollapsed : ''}`}
-      >
-        <LibraryHeader isCollapsed={isCollapsed} onCollapse={handleCollapse} />
-        {!isCollapsed && <LibrarySearch />}
-        <LibraryList />
-      </div>
-    </LibrarySortProvider>
+    <div
+      className={`${styles.library} ${isCollapsed ? styles.libraryCollapsed : ''}`}
+    >
+      <LibraryHeader isCollapsed={isCollapsed} onCollapse={handleCollapse} />
+      {!isCollapsed && <LibrarySearch />}
+      <LibraryList />
+    </div>
   );
 };
 
