08cc3d016aa91589125b555a4a91998d51d0cbcc
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-21 16:10:44 +0500

    ✨ Add library search feature

--- client/src/features/library/LibrarySearch/LibrarySearch.tsx
+++ client/src/features/library/LibrarySearch/LibrarySearch.tsx
@@ -1,13 +1,43 @@
 import styles from './LibrarySearch.module.scss';
-import LibrarySort from '../LibrarySort/LibrarySort.tsx';
-import LibrarySearchForm from './LibrarySearchForm.tsx';
+import { RiSearchLine } from 'react-icons/ri';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../app/hooks.ts';
+import { searchLibraryItems } from '../librarySlice.ts';
 
 const LibrarySearch = () => {
+  // const { searchQuery } = useAppSelector((state) => state.library);
+  const dispatch = useAppDispatch();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    dispatch(searchLibraryItems(e.target.value));
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  const handleToggleForm = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div className={styles.librarySearch}>
-      <LibrarySearchForm />
-      <LibrarySort />
-    </div>
+    <form className={styles.librarySearch} onSubmit={handleSubmit}>
+      <button
+        className={`${styles.librarySearchButton} ${isOpen ? styles.librarySearchButtonHover : ''}`}
+        onClick={handleToggleForm}
+      >
+        <RiSearchLine />
+      </button>
+      <input
+        className={`${styles.librarySearchInput} ${isOpen ? styles.librarySearchInputOpen : ''}`}
+        type="text"
+        placeholder="Search in Your Library"
+        onChange={handleInputChange}
+      />
+    </form>
   );
 };
 
