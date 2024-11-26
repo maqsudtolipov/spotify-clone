df73250ef9bd1f6302aef82955cdbaa7701d86cb
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-26 09:55:31 +0500

    ✨ Change background gradient color on history item hover

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,9 +1,30 @@
+import styles from './Home.module.scss';
 import History from '../features/history/History.tsx';
+import { useState } from 'react';
 
 const Home = () => {
+  const [gradientColor, setGradientColor] = useState<string>();
+
+  const handleNewColor = () => {
+    const randomHexColor =
+      '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
+    setGradientColor(randomHexColor);
+  };
+
   return (
-    <div className="px-4 py-5">
-      <History />
+    <div
+      className={styles.home}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgb(18, 18, 18) 40vh),
+        linear-gradient(
+        ${gradientColor},
+        ${gradientColor} 40vh,
+        transparent 40vh,
+        transparent 100%
+    )`,
+      }}
+    >
+      <History handleNewColor={handleNewColor} />
     </div>
   );
 };
