180b95f1a7de9794c165ff4c6c2ab97a54ddf157
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-26 11:03:05 +0500

    🎨 Fix main scroll

--- client/src/layout/Footer/Footer.tsx
+++ client/src/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { RiGithubFill } from 'react-icons/ri';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <span>Made w️ith ❤️ my Maqsud. *For learning purposes only.</span>
+      <span>Made w️ith ❤️ by Maqsud. *For learning purposes only.</span>
       <a href="https://github.com/maqsudtolipov/spotify-clone/" target="_blank">
         <RiGithubFill />
       </a>
