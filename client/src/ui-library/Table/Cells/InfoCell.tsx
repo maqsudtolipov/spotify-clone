ebf1891684394f1fdab23e0d96300e7d6846b1be
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-28 08:31:02 +0500

    ⚙️ Refactor code

--- client/src/ui-library/Table/Cells/InfoCell.tsx
+++ client/src/ui-library/Table/Cells/InfoCell.tsx
@@ -4,15 +4,16 @@ import TableCell from '../TableCell.tsx';
 interface InfoCellProps {
   img: string;
   name: string;
+  artist: string;
 }
 
-const InfoCell = ({ img, name }: InfoCellProps) => {
+const InfoCell = ({ img, name, artist }: InfoCellProps) => {
   return (
     <TableCell className={styles.infoCell}>
       <img src={img} alt={name} />
       <div className={styles.infoCellDescription}>
         <span className={styles.infoCellName}>{name}</span>
-        <span className={styles.infoCellArtist}>{name.split(' ')[0]}</span>
+        <span className={styles.infoCellArtist}>{artist}</span>
       </div>
     </TableCell>
   );
--- client/src/ui-library/Table/TableCell.tsx
+++ client/src/ui-library/Table/TableCell.tsx
@@ -2,21 +2,21 @@ import { ReactNode } from 'react';
 
 interface TableCellProps {
   className?: string;
-  alightRight?: boolean;
+  alignRight?: boolean;
   minimize?: boolean;
   children: ReactNode;
 }
 
 const TableCell = ({
   className,
-  alightRight,
+  alignRight,
   minimize,
   children,
   ...rest
 }: TableCellProps) => {
   return (
     <td
-      className={`${className ? className : ''} ${alightRight ? 'text-right' : ''} ${minimize ? 'w-1' : ''}`}
+      className={`${className ? className : ''} ${alignRight ? 'text-right' : ''} ${minimize ? 'w-1' : ''}`}
       {...rest}
     >
       {children}
