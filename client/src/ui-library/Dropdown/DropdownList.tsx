fe2fe4946bf5e0a9d965792166e91cb1a041ec71
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-28 11:31:44 +0500

    ✨ Add action buttons

--- client/src/ui-library/Dropdown/DropdownList.tsx
+++ client/src/ui-library/Dropdown/DropdownList.tsx
@@ -4,15 +4,24 @@ import { DropdownContext } from './Dropdown.tsx';
 import useOutsideClick from '../../hooks/useOutsideClick.tsx';
 
 interface DropdownListProps {
+  position?: 'bottom-left' | 'bottom-right';
   children: ReactNode;
 }
 
-const DropdownList = ({ children, ...rest }: DropdownListProps) => {
+const DropdownList = ({
+  position = 'bottom-left',
+  children,
+  ...rest
+}: DropdownListProps) => {
   const { isOpen, closeDropdown } = useContext(DropdownContext);
   const ref = useOutsideClick(closeDropdown);
 
   return isOpen ? (
-    <ul ref={ref} className={styles.listContainer} {...rest}>
+    <ul
+      ref={ref}
+      className={`${styles.listContainer} ${styles[position]}`}
+      {...rest}
+    >
       {children}
     </ul>
   ) : null;
