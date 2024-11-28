3b4921fb761839ed5b600bf80c076ccb9e2bb5ed
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-28 11:34:17 +0500

    ⚙️ Refactor code

--- client/src/features/artist/Artist.tsx
+++ client/src/features/artist/Artist.tsx
@@ -24,7 +24,6 @@ const Artist = () => {
       >
         <ArtistActions />
         <div className="p-5">
-          <h2 className="text-3xl text-bold mb-4">Artist</h2>
           <ArtistTable />
         </div>
       </div>
