b1677e6043b7273b8eaed177b70e476b1ef349c5
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-28 14:24:30 +0500

    ⚙️ Move gradient background to separate component

--- client/src/features/artist/Artist.tsx
+++ client/src/features/artist/Artist.tsx
@@ -1,6 +1,7 @@
 import ArtistTable from './ArtistTable.tsx';
 import ArtistHeader from './ArtistHeader.tsx';
 import PlayHeader from '../../components/PlayHeader/PlayHeader.tsx';
+import GradientBackground from '../../components/GradientBackground/GradientBackground.tsx';
 
 const Artist = () => {
   const color =
@@ -11,22 +12,12 @@ const Artist = () => {
   return (
     <div>
       <ArtistHeader color={color} />
-      <div
-        style={{
-          backgroundImage: `linear-gradient(rgba(23, 23, 23, 0.5), rgb(23, 23, 23) 40vh),
-        linear-gradient(
-        ${color},
-        ${color} 40vh,
-        transparent 40vh,
-        transparent 100%
-    )`,
-        }}
-      >
+      <GradientBackground color={color}>
         <PlayHeader />
         <div className="p-5 pt-0">
           <ArtistTable />
         </div>
-      </div>
+      </GradientBackground>
     </div>
   );
 };
--- client/src/features/playlist/Playlist.tsx
+++ client/src/features/playlist/Playlist.tsx
@@ -1,6 +1,7 @@
 import PlayHeader from '../../components/PlayHeader/PlayHeader.tsx';
 import PlaylistTable from './PlaylistTable.tsx';
 import PlaylistHeader from './PlaylistHeader.tsx';
+import GradientBackground from '../../components/GradientBackground/GradientBackground.tsx';
 
 const Playlist = () => {
   const color =
@@ -11,22 +12,12 @@ const Playlist = () => {
   return (
     <div>
       <PlaylistHeader color={color} />
-      <div
-        style={{
-          backgroundImage: `linear-gradient(rgba(23, 23, 23, 0.5), rgb(23, 23, 23) 40vh),
-        linear-gradient(
-        ${color},
-        ${color} 40vh,
-        transparent 40vh,
-        transparent 100%
-    )`,
-        }}
-      >
+      <GradientBackground color={color}>
         <PlayHeader />
         <div className="p-5 pt-0">
           <PlaylistTable />
         </div>
-      </div>
+      </GradientBackground>
     </div>
   );
 };
