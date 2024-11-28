9cc82b7bed181e1aa89cd0cda5c9e4a93a774b6a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-11-28 09:35:20 +0500

    ✨ Create artist header

--- client/src/features/artist/ArtistHeader.tsx
+++ client/src/features/artist/ArtistHeader.tsx
@@ -1,10 +1,11 @@
 import styles from './Artist.module.scss';
+import { RiVerifiedBadgeFill } from 'react-icons/ri';
 
 const ArtistHeader = () => {
   const randomHexColor =
     '#' +
     ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0') +
-    '7f';
+    '4d';
 
   return (
     <div
@@ -13,7 +14,12 @@ const ArtistHeader = () => {
         background: `linear-gradient(${randomHexColor}, ${randomHexColor}), linear-gradient(#171717, #171717)`,
       }}
     >
-      red
+      <div className={styles.artistVerified}>
+        <RiVerifiedBadgeFill />
+        <span>Verified Artist</span>
+      </div>
+      <h1 className={styles.artistName}>Full Gates</h1>
+      <span>1,234 listeners</span>
     </div>
   );
 };
