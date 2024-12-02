947200100d81f1122259d8d5d2696a10fe8a655c
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-02 13:12:14 +0500

    ✨ Create Songs tab for search page

--- client/src/features/search/Search.tsx
+++ client/src/features/search/Search.tsx
@@ -1,14 +1,31 @@
 import SearchTabs from './SearchTabs.tsx';
 import Heading2 from '../../ui-library/Typography/Heading2.tsx';
+import { useState } from 'react';
+import SearchSongs from './sub/SearchSongs.tsx';
 
 const Search = () => {
+  const [tab, setTab] = useState<
+    'all' | 'artists' | 'playlists' | 'songs' | 'profiles'
+  >('all');
+
+  const handleTabChange = (tab): never => {
+    setTab(tab);
+  };
+
   return (
     <div className="px-4 py-5">
-      <SearchTabs />
-      <div className="grid grid-cols-2">
-        <Heading2>Top Result</Heading2>
-        <Heading2>Songs</Heading2>
-      </div>
+      <SearchTabs handleTabChange={handleTabChange} />
+
+      {tab === 'all' && (
+        <div className="grid grid-cols-[2fr_3fr]">
+          <Heading2>Top Result</Heading2>
+          <Heading2>Songs</Heading2>
+        </div>
+      )}
+      {tab === 'artists' && <p>Artists</p>}
+      {tab === 'playlists' && <p>Playlists</p>}
+      {tab === 'songs' && <SearchSongs />}
+      {tab === 'profiles' && <p>Profiles</p>}
     </div>
   );
 };
--- client/src/features/search/SearchTabs.tsx
+++ client/src/features/search/SearchTabs.tsx
@@ -2,15 +2,31 @@ import Tabs from '../../ui-library/Tabs/Tabs';
 import TabsList from '../../ui-library/Tabs/TabsList';
 import Tab from '../../ui-library/Tabs/Tab';
 
-const SearchTabs = () => {
+interface SearchTabsProps {
+  handleTabChange: (
+    tab: 'all' | 'artists' | 'playlists' | 'songs' | 'profiles',
+  ) => void;
+}
+
+const SearchTabs = ({ handleTabChange }: SearchTabsProps) => {
   return (
     <Tabs hideUnselected={false} defaultValue="all">
-      <TabsList className="mb-5">
-        <Tab value="all">All</Tab>
-        <Tab value="song">Songs</Tab>
-        <Tab value="artist">Artists</Tab>
-        <Tab value="playlist">Playlists</Tab>
-        <Tab value="profiles">Profiles</Tab>
+      <TabsList className="mb-6">
+        <Tab value="all" onClick={handleTabChange}>
+          All
+        </Tab>
+        <Tab value="artists" onClick={handleTabChange}>
+          Artists
+        </Tab>
+        <Tab value="playlists" onClick={handleTabChange}>
+          Playlists
+        </Tab>
+        <Tab value="songs" onClick={handleTabChange}>
+          Songs
+        </Tab>
+        <Tab value="profiles" onClick={handleTabChange}>
+          Profiles
+        </Tab>
       </TabsList>
     </Tabs>
   );
