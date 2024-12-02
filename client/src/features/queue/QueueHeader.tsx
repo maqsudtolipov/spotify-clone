a9a58da7628980486a96ee46df692f752935c1fa
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-02 16:38:05 +0500

    ✨ Create header

--- client/src/features/queue/QueueHeader.tsx
+++ client/src/features/queue/QueueHeader.tsx
@@ -1,11 +1,15 @@
 import styles from './Queue.module.scss';
 import { RiCloseLargeFill } from 'react-icons/ri';
+import IconButton from '../../ui-library/IconButton/IconButton';
 
 const QueueHeader = () => {
   return (
     <header className={styles.header}>
       <span>Queue</span>
-      <RiCloseLargeFill />
+
+      <IconButton>
+        <RiCloseLargeFill />
+      </IconButton>
     </header>
   );
 };
