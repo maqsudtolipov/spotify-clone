90de0d81ad1d66f1a01c7d13ae375e2bcc77de01
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-13 11:14:35 +0500

    ✨ Setup mongoose

--- server/server.js
+++ server/server.js
@@ -1,25 +1,33 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
-});
+// DotENV
+dotenv.config({ path: "./.env" });
 
 // Uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log("Uncaught Exception: ", err.message);
-  console.log("Closing server now...");
+  console.log("🔴 Closing server now...");
   process.exit(1);
 });
 
+// Connect to mongoDb
+const DB = process.env.DB_URL.replace("<db_password>", process.env.DB_PASS);
+mongoose.connect(DB).then(() => {
+  console.log(`🟢 DATABASE CONNECTED`);
+});
+
+// Server
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`),
+  console.log(`🟡 Server running on port ${PORT}`),
 );
 
 // Uncaught Rejection
 process.on("unhandledRejection", (err) => {
   console.log(err);
-  console.log("Closing server now...");
+  console.log("🔴 Closing server now...");
   server.close(() => {
     process.exit(1);
   });
