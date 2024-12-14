13e64d3b2bceeb8ee73e027ef60d5e56f81c2a1a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-14 12:27:18 +0500

    ✨ Validate input date for sign-up route

--- server/src/feature/auth/userModel.js
+++ server/src/feature/auth/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -10,10 +11,21 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Please provide a valid email address"],
       unique: [true, "User with this email already exists"],
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please provide a valid email address",
+      ],
     },
     password: {
       type: String,
       required: [true, "Please provide a password"],
+      minlength: [8, "Password must be at least 8 characters long"],
+      maxLength: [16, "Password must be at most 16 characters long"],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character ($, !, %, &, *).",
+      ],
+      select: false,
     },
     passwordConfirm: {
       type: String,
@@ -24,13 +36,29 @@ const userSchema = new mongoose.Schema(
         },
         message: "Passwords do not match",
       },
-    }
+      select: false,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
   },
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   },
 );
+
+// Methods
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
