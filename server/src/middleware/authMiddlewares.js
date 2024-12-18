234b50a36bd9724e55e9739309237e59e4c5b3d1
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-18 11:41:51 +0500

    🚀 Check if access token is expired or invalid

--- server/src/middleware/authMiddlewares.js
+++ server/src/middleware/authMiddlewares.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const AppError = require("../utils/AppError");
 
 const ensureAuthenticated = async (req, res, next) => {
@@ -7,7 +8,28 @@ const ensureAuthenticated = async (req, res, next) => {
     return next(new AppError("Access token not found", 401));
   }
 
-  next();
+  try {
+    const decodedAccessToken = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET,
+    );
+
+    console.log(decodedAccessToken);
+
+    next();
+  } catch (e) {
+    if (e instanceof jwt.TokenExpiredError) {
+      return res
+        .status(401)
+        .json({ message: "Access token expired", code: "AccessTokenExpired" });
+    } else if (e instanceof jwt.JsonWebTokenError) {
+      return res
+        .status(401)
+        .json({ message: "Access token invalid", code: "AccessTokenInvalid" });
+    }
+
+    next(e);
+  }
 };
 
 module.exports = { ensureAuthenticated };
