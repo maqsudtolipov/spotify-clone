991e66f1e41712fd16bf2cae873032b4c46984c5
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-21 13:09:56 +0500

    🎨 Design login page

--- client/src/features/auth/Login.tsx
+++ client/src/features/auth/Login.tsx
@@ -1,9 +1,29 @@
+import styles from './Login.module.scss';
 import AuthContainer from './AuthContainer.tsx';
 
 const Login = () => {
   return (
     <AuthContainer>
-      Login
+      <form className={styles.form} action="">
+        <p className={styles.title}>Spotify</p>
+
+        <input
+          className={styles.input}
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+        <input
+          className={styles.input}
+          type="password"
+          name="password"
+          placeholder="Enter your password"
+        />
+
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </form>
     </AuthContainer>
   );
 };
