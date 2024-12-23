6f2ebbdafd9db3f96a830e801bcfc0ee96ae8cc7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-23 07:58:11 +0500

    ✨ Style login page

--- client/src/features/auth/Login.tsx
+++ client/src/features/auth/Login.tsx
@@ -1,24 +1,63 @@
+import { useForm } from 'react-hook-form';
 import styles from './Login.module.scss';
 import AuthContainer from './AuthContainer.tsx';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const watchedPassword = watch('password');
+  const validatePassword = (password: string) => {
+    // if (password.length < 8) return 'Minimum 8 characters.';
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password,
+    );
+  };
+
+  const watchedEmail = watch('email');
+  const validateEmail = (email: string) => {
+    return  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
   return (
     <AuthContainer>
-      <form className={styles.form} action="">
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
         <p className={styles.title}>Spotify</p>
+        <div className={styles.container}>
+          <input
+            className={`${styles.input} ${validateEmail(watchedEmail) ? styles.inputValid : ''}`}
+            type="email"
+            placeholder="Enter your email"
+            {...register('email', { required: 'Email is required' })}
+          />
+        </div>
 
-        <input
-          className={styles.input}
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-        />
-        <input
-          className={styles.input}
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
+        <div className={styles.container}>
+          <input
+            className={`${styles.input} ${validatePassword(watchedPassword) ? styles.inputValid : ''}`}
+            type="password"
+            placeholder="Enter your password"
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 4,
+                message: 'Min length 4',
+              },
+            })}
+          />
+          <p className={styles.message}>{validatePassword(watchedPassword)}</p>
+        </div>
 
         <button className={styles.button} type="submit">
           Login
