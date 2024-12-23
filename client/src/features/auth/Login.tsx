eefdb6b6fd43207bd156fe3b242937baa915be4f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-23 09:43:50 +0500

    🧹 Refactor Login component

--- client/src/features/auth/Login.tsx
+++ client/src/features/auth/Login.tsx
@@ -11,8 +11,18 @@ interface FormInput {
   password: string;
 }
 
+const validatePassword = (password: string) => {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+    password,
+  );
+};
+
+const validateEmail = (email: string) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
 const Login = () => {
-  const { isAuth } = useAppSelector((state) => state.user);
+  const { isAuth, status } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
 
   const { register, handleSubmit, watch } = useForm({
@@ -21,63 +31,53 @@ const Login = () => {
       password: '',
     },
   });
-
   const watchedPassword = watch('password');
-  const validatePassword = (password: string) => {
-    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-      password,
-    );
-  };
-
   const watchedEmail = watch('email');
-  const validateEmail = (email: string) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  };
 
   const handleFormSubmit = async (input: FormInput) => {
     dispatch(login(input));
   };
 
   if (isAuth) return <Navigate to="/" />;
+  else
+    return (
+      <AuthContainer>
+        <form
+          className={styles.form}
+          onSubmit={handleSubmit((data) => handleFormSubmit(data))}
+        >
+          <p className={styles.title}>Spotify</p>
+          <div className={styles.container}>
+            <input
+              className={`${styles.input} ${validateEmail(watchedEmail) ? styles.inputValid : ''}`}
+              type="email"
+              placeholder="Enter your email"
+              {...register('email', { required: 'Email is required' })}
+            />
+          </div>
 
-  return (
-    <AuthContainer>
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit((data) => handleFormSubmit(data))}
-      >
-        <p className={styles.title}>Spotify</p>
-        <div className={styles.container}>
-          <input
-            className={`${styles.input} ${validateEmail(watchedEmail) ? styles.inputValid : ''}`}
-            type="email"
-            placeholder="Enter your email"
-            {...register('email', { required: 'Email is required' })}
-          />
-        </div>
+          <div className={styles.container}>
+            <input
+              className={`${styles.input} ${validatePassword(watchedPassword) ? styles.inputValid : ''}`}
+              type="password"
+              placeholder="Enter your password"
+              {...register('password', {
+                required: 'Password is required',
+              })}
+            />
+            <p className={styles.message}>
+              {!validatePassword(watchedPassword) &&
+                'Min 8 chars with uppercase, lowercase, number, and special ($, !, %, &, *).'}
+            </p>
+          </div>
 
-        <div className={styles.container}>
-          <input
-            className={`${styles.input} ${validatePassword(watchedPassword) ? styles.inputValid : ''}`}
-            type="password"
-            placeholder="Enter your password"
-            {...register('password', {
-              required: 'Password is required',
-            })}
-          />
-          <p className={styles.message}>
-            {!validatePassword(watchedPassword) &&
-              'Min 8 chars with uppercase, lowercase, number, and special ($, !, %, &, *).'}
-          </p>
-        </div>
-
-        <button className={styles.button} type="submit">
-          {status === 'pending' && <RiLoaderFill />}
-          Login
-        </button>
-      </form>
-    </AuthContainer>
-  );
+          <button className={styles.button} type="submit">
+            {status === 'pending' && <RiLoaderFill />}
+            Login
+          </button>
+        </form>
+      </AuthContainer>
+    );
 };
 
 export default Login;
