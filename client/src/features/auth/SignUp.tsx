f4aaebfb61350f9c3651082ff5f4b2735e49a17c
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2024-12-23 15:00:30 +0500

    🚀 Validate signup inputs

--- client/src/features/auth/SignUp.tsx
+++ client/src/features/auth/SignUp.tsx
@@ -14,33 +14,37 @@ interface FormInput {
 }
 
 const validateInput = (data: FormInput) => {
+  const validInputs: string[] = [];
   const errorMessages: string[] = [];
 
   // 1. Validate name
   if (data.name.length < 3 || data.name.length > 24) errorMessages.push('Name must be 3–24 characters.');
-  if (!data.name) errorMessages.push('Please provide your name.');
+  else validInputs.push('name');
 
   // 2. Validate email
-  if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) errorMessages.push('Please provide a valid email address.');
+  if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) errorMessages.push('Please provide a valid email address.')
+  else validInputs.push('email');
 
   // 3. Validate password
-  if (data.name.length < 8 || data.name.length > 16) errorMessages.push('Password must be 8–16 characters.');
-  if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+  if (data.password.length < 8 || data.password.length > 16) errorMessages.push('Password must be 8–16 characters.');
+  else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
     data.password,
   )) {
     errorMessages.push('Password must include an uppercase letter, a lowercase letter, a number, and a special character.');
   }
+  else validInputs.push('password');
 
   // 4. Validate confirmPassword
-  if (data.password !== data.passwordConfirm) errorMessages.push('Passwords do not match.');
+  if (data.password !== data.passwordConfirm) errorMessages.push('Passwords do not match.')
+  else validInputs.push('passwordConfirm');
   
-  return errorMessages;
+  return { validInputs, errorMessages };
 }
 
 const Login = () => {
   const { isAuth, status } = useAppSelector((state) => state.user);
   const [errorMessages, setErrorMessages] = useState<string[]>([]);
-  // const dispatch = useAppDispatch();
+  const [validInputs, setValidInputs] = useState<string[]>([]);
 
   const { register, handleSubmit } = useForm({
     defaultValues: {
@@ -52,11 +56,12 @@ const Login = () => {
   });
 
   const handleFormSubmit = async (input: FormInput) => {
-    if (validateInput(input).length === 0) {
-      // dispatch(login(input));
+    if (validateInput(input).errorMessages.length === 0) {
       setErrorMessages([])
+      setValidInputs(['name', 'email', 'password', 'passwordConfirm'])
     } else {
-      setErrorMessages(validateInput(input));
+      setValidInputs(validateInput(input).validInputs);
+      setErrorMessages(validateInput(input).errorMessages);
     }
   };
 
@@ -72,42 +77,40 @@ const Login = () => {
 
           <div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${validInputs.includes('name') ? styles.inputValid : ''}`}
               type="text"
               placeholder="Enter your name"
-              minLength={3}
-              maxLength={24}
+              required={true}
               {...register('name')}
             />
           </div>
 
           <div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${validInputs.includes('email') ? styles.inputValid : ''}`}
               type="email"
               placeholder="Enter your email"
+              required={true}
               {...register('email')}
             />
           </div>
 
           <div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${(validInputs.includes('password') && validInputs.includes('passwordConfirm')) ? styles.inputValid : ''}`}
               type="password"
               placeholder="Enter your password"
-              minLength={8}
-              maxLength={16}
+              required={true}
               {...register('password')}
             />
           </div>
 
           <div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${(validInputs.includes('password') && validInputs.includes('passwordConfirm')) ? styles.inputValid : ''}`}
               type="password"
               placeholder="Confirm your password"
-              minLength={8}
-              maxLength={16}
+              required={true}
               {...register('passwordConfirm')}
             />
           </div>
