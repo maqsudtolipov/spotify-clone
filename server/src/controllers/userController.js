700e8ed132666c890eae2df64993f5bc6391ed42
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-08 09:24:31 +0500

    🚀 Create separate middleware for user storage

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,37 +1,4 @@
 const User = require("../models/userModel");
-const multer = require("multer");
-const sharp = require("sharp");
-
-const storage = multer.memoryStorage();
-
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.split("/")[0] === "image") {
-    cb(null, true);
-  } else {
-    cb(new Error("Only images are allowed!"));
-  }
-};
-
-const upload = multer({ storage, fileFilter });
-
-exports.uploadUserImg = upload.single("userImg");
-
-exports.resizeUserImg = async (req, res, next) => {
-  try {
-    if (!req.file) return next();
-
-    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-
-    req.file.buffer = await sharp(req.file.buffer)
-      .resize(512, 512)
-      .toFormat("jpeg")
-      .toBuffer();
-
-    next();
-  } catch (e) {
-    next(e);
-  }
-};
 
 exports.getAll = async (req, res, next) => {
   try {
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,10 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const authorize = require("../middlewares/authorize");
-const {
-  uploadUserImg,
-  resizeUserImg,
-} = require("../controllers/userController");
+const { uploadUserImg, resizeUserImg } = require("../middlewares/userStorage");
 
 const router = express.Router();
 
