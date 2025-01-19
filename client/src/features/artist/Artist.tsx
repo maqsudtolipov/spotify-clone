43ff7cd68c8d76d3de3369295160758a9f73e100
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-19 14:52:28 +0500

    🚀 Show artist color on client

--- client/src/features/artist/Artist.tsx
+++ client/src/features/artist/Artist.tsx
@@ -2,23 +2,33 @@ import ArtistTable from './ArtistTable.tsx';
 import ArtistHeader from './ArtistHeader.tsx';
 import PlayHeader from '../../components/PlayHeader/PlayHeader.tsx';
 import GradientBackground from '../../components/GradientBackground/GradientBackground.tsx';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
+import { useEffect } from 'react';
+import { getArtist } from './artistThunks.ts';
+import LoadingScreen from '../../components/LoadingScreen/LoadingScreen.tsx';
 
 const Artist = () => {
-  const color =
-    '#' +
-    ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0') +
-    '4d';
+  const { id } = useParams();
+  const data = useAppSelector((state) => state.artist.data);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (id) dispatch(getArtist(id));
+  }, [id]);
+
+  if (!data) return <LoadingScreen />;
 
   return (
-    <div>
-      <ArtistHeader color={color} />
-      <GradientBackground color={color}>
+    <>
+      <ArtistHeader />
+      <GradientBackground color={data.color}>
         <PlayHeader />
         <div className="p-5 pt-0">
           <ArtistTable />
         </div>
       </GradientBackground>
-    </div>
+    </>
   );
 };
 
--- client/src/features/artist/ArtistHeader.tsx
+++ client/src/features/artist/ArtistHeader.tsx
@@ -1,34 +1,22 @@
 import styles from './Artist.module.scss';
 import { RiVerifiedBadgeFill } from 'react-icons/ri';
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
-import { getArtist } from './artistThunks.ts';
-import { useParams } from 'react-router-dom';
-import LoadingScreen from '../../components/LoadingScreen/LoadingScreen.tsx';
+import { useAppSelector } from '../../app/hooks.ts';
 
 const ArtistHeader = () => {
-  const { id } = useParams();
-  const data = useAppSelector((state) => state.artist.data);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(getArtist(id));
-  }, [id]);
-
-  if (!data) return <LoadingScreen />;
+  const { color, name } = useAppSelector((state) => state.artist.data);
 
   return (
     <header
       className={styles.artistHeader}
       style={{
-        background: `linear-gradient(${data.color}, ${data.color}), linear-gradient(#171717, #171717)`,
+        background: `linear-gradient(${color}, ${color}), linear-gradient(#171717, #171717)`,
       }}
     >
       <div className={styles.artistVerified}>
         <RiVerifiedBadgeFill />
         <span>Verified Artist</span>
       </div>
-      <h1 className={styles.artistName}>{data.name}</h1>
+      <h1 className={styles.artistName}>{name}</h1>
       <span>1234 listeners</span>
     </header>
   );
