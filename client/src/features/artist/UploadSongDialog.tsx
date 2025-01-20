bd9b28ddbcdf860277d24caa013883a1fea87c3c
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-20 12:58:52 +0500

    🚀 Store input files inside FormData

--- client/src/features/artist/UploadSongDialog.tsx
+++ client/src/features/artist/UploadSongDialog.tsx
@@ -5,19 +5,38 @@ import DialogTrigger from '../../ui-library/Dialog/DialogTrigger.tsx';
 import TransparentButton from '../../components/PlayHeader/TransparentButton.tsx';
 import Input from '../../ui-library/Input/Input.tsx';
 import Button from '../../ui-library/Button/Button';
+import { FormEvent, useRef } from 'react';
 
 const UploadSongDialog = () => {
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    if (formRef.current) {
+      e.preventDefault();
+
+      const formData = new FormData(formRef.current);
+
+      for (let [key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`);
+      }
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger>
         <TransparentButton text="Upload Song" onClick={() => {}} />
       </DialogTrigger>
       <DialogContent>
-        <form action="" className={styles.dialogForm}>
-          <Input type="file" placeholder="Song cover img"></Input>
-          <Input type="file" placeholder="Song file"></Input>
-          <Input type="text" placeholder="Song name"></Input>
-          <Button>Upload</Button>
+        <form
+          ref={formRef}
+          className={styles.dialogForm}
+          onSubmit={handleFormSubmit}
+        >
+          <Input type="file" name="img" placeholder="Song cover img"></Input>
+          <Input type="file" name="song" placeholder="Song file"></Input>
+          <Input type="text" name="name" placeholder="Song name"></Input>
+          <Button type="submit">Upload</Button>
         </form>
       </DialogContent>
     </Dialog>
--- client/src/ui-library/Input/Input.tsx
+++ client/src/ui-library/Input/Input.tsx
@@ -2,14 +2,19 @@ import styles from './Input.module.scss';
 
 interface InputProps {
   type: string;
+  name: string;
   placeholder?: string;
 }
 
-const Input = ({ type, placeholder }: InputProps) => {
+const Input = ({ type, name, placeholder, ...rest }: InputProps) => {
   return (
-    <>
-      <input className={styles.input} type={type} placeholder={placeholder} />
-    </>
+    <input
+      className={styles.input}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      {...rest}
+    />
   );
 };
 
