eccc3f389068f9fdb828d2ce9772cfe4af41fdb1
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-22 12:35:28 +0500

    🧪 Add tests for like/dislike controller

--- server/test/controllers/songController.test.js
+++ server/test/controllers/songController.test.js
@@ -11,6 +11,7 @@ const {
   connectToDatabase,
   cleanupDatabaseAndDisconnect,
 } = require("../helpers/databaseHelpers");
+const Playlist = require("../../src/models/playlistModel");
 
 let server;
 beforeAll(async () => {
@@ -25,6 +26,7 @@ afterAll(async () => {
 });
 
 describe("songController", () => {
+  // TODO: test coverage is missing
   describe("uploadSong", () => {
     let userIds, accessToken;
 
@@ -110,6 +112,19 @@ describe("songController", () => {
       songId = song.id;
     });
 
+    it("should add song to liked songs and return updated playlist", async () => {
+      const res = await request(app)
+        .post(`/api/songs/${songId}/like`)
+        .set("Cookie", [`accessToken=${accessToken}`]);
+
+      const updatedPlaylist = await Playlist.findOne({ user: userIds[1] });
+      expect(updatedPlaylist.songs.length).toEqual(1);
+
+      expect(res.status).toBe(200);
+      expect(res.body.status).toBe("success");
+      expect(res.body.likedSongs.length).toEqual(1);
+    });
+
     it("should fail if song id is invalid", async () => {
       const res = await request(app)
         .post(`/api/songs/wrongId/like`)
@@ -120,10 +135,88 @@ describe("songController", () => {
       expect(res.body.message).toMatch(/Invalid _id: wrongId/i);
     });
 
-    it("should fail if dong does not exist", async () => {
+    it("should fail if song does not exist", async () => {
       // The id is randomly generated
       const res = await request(app)
         .post(`/api/songs/67909814d606821ac21d1b78/like`)
+        .set("Cookie", [`accessToken=${accessToken}`]);
+
+      expect(res.status).toBe(404);
+      expect(res.body.status).toBe("fail");
+      expect(res.body.message).toMatch(/Song not found/i);
+    });
+  });
+
+  describe("dislikeSong", () => {
+    let userIds, accessToken, songId;
+
+    beforeAll(async () => {
+      // Create users
+      const res = await createUsersAndLogin(
+        [
+          {
+            name: "Makena",
+            email: "makena@example.com",
+            password: "Pa$$1234",
+            passwordConfirm: "Pa$$1234",
+            role: "artist",
+          },
+          {
+            name: "Steve",
+            email: "steve@example.com",
+            password: "Pa$$1234",
+            passwordConfirm: "Pa$$1234",
+          },
+        ],
+        1,
+      );
+
+      userIds = res.userIds;
+      accessToken = res.accessToken;
+
+      const songInput = {
+        name: "Pale Garden",
+        song: "pale-garden.mp3",
+        img: "pale-garden.png",
+        artist: userIds[0],
+        duration: 120,
+      };
+
+      const song = await Song.create(songInput);
+      songId = song.id;
+
+      await request(app)
+        .post(`/api/songs/${songId}/like`)
+        .set("Cookie", [`accessToken=${accessToken}`]);
+    });
+
+    it("should remove the song from likedSongs playlist and return updated playlist", async () => {
+      const res = await request(app)
+        .post(`/api/songs/${songId}/dislike`)
+        .set("Cookie", [`accessToken=${accessToken}`]);
+
+      const updatedPlaylist = await Playlist.findOne({ user: userIds[1] });
+      expect(updatedPlaylist.songs.length).toEqual(0);
+
+      expect(res.status).toBe(200);
+      expect(res.body.status).toBe("success");
+      expect(res.body.likedSongs.length).toEqual(0);
+    });
+
+    it("should fail if song id is invalid", async () => {
+      const res = await request(app)
+        .post(`/api/songs/wrongId/dislike`)
+        .set("Cookie", [`accessToken=${accessToken}`]);
+
+      expect(res.status).toBe(400);
+      expect(res.body.status).toBe("fail");
+      expect(res.body.message).toMatch(/Invalid _id: wrongId/i);
+    });
+
+    it("should fail if song does not exist", async () => {
+      // The id is randomly generated
+      const res = await request(app)
+        .post(`/api/songs/67909814d606821ac21d1b78/dislike`)
         .set("Cookie", [`accessToken=${accessToken}`]);
 
       expect(res.status).toBe(404);
