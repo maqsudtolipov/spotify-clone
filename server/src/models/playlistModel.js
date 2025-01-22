06900c0e2e4ea59969fe2cf28fd9c9093a3cdc5f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-22 08:16:54 +0500

    🚀 Create playlist model

---
+++ server/src/models/playlistModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const playlistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a playlist name"],
+      minLength: [3, "Name must be at least 3 characters long"],
+      maxLength: [24, "Name must be at most 24 characters long"],
+    },
+    description: {
+      type: String,
+      minLength: [3, "Name must be at least 3 characters long"],
+      maxLength: [50, "Name must be at most 50 characters long"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    songs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+    isLikedSongs: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  },
+);
+
+const Playlist = new mongoose.Model("Playlist", playlistSchema);
+
+module.exports = Playlist;
