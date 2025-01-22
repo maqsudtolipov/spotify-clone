86d50c665d7cec61a3bf47350668e5fbde19a857
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-22 11:33:16 +0500

    🚀 Update error handler middleware

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -30,12 +30,18 @@ const productionError = (err, res) => {
   }
 };
 
-const developmentError = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
+const developmentError = (prodError, devError, res) => {
+  res.status(devError.statusCode).json({
+    prod: {
+      status: prodError.status,
+      message: prodError.message,
+    },
+    dev: {
+      status: devError.status,
+      message: devError.message,
+      error: devError,
+      stack: devError.stack,
+    },
   });
 };
 
@@ -43,16 +49,17 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  let error = Object.assign(err);
+  let prodErr = Object.assign(err);
 
-  if (error.name === "ValidationError") error = handleValidationError(error);
-  if (error instanceof SyntaxError) error = handleSyntaxError(error);
-  if (error.code === 11000) error = handleDuplicationError(error);
-  if (error.name === "CastError") error = handleCastError(error);
+  if (prodErr.name === "ValidationError")
+    prodErr = handleValidationError(prodErr);
+  if (prodErr instanceof SyntaxError) prodErr = handleSyntaxError(prodErr);
+  if (prodErr.code === 11000) prodErr = handleDuplicationError(prodErr);
+  if (prodErr.name === "CastError") prodErr = handleCastError(prodErr);
 
   if (process.env.NODE_ENV === "development") {
-    developmentError(err, res);
+    developmentError(prodErr, err, res);
   } else if (process.env.NODE_ENV === "production") {
-    productionError(error, res);
+    productionError(prodErr, res);
   }
 };
