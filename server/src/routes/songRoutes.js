bdd3c41cc92afff427b868ba0152fe81e3c8a165
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-23 06:19:37 +0500

    🚀 Add song img and file processor middlewares

--- server/src/routes/songRoutes.js
+++ server/src/routes/songRoutes.js
@@ -2,11 +2,19 @@ const express = require("express");
 const songController = require("../controllers/songController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const authorize = require("../middlewares/authorize");
+const songsStorage = require("../middlewares/songsStorage");
 const router = express.Router();
 
 router
   .route("/:id/")
-  .patch(ensureAuthenticated, authorize(["artist"]), songController.updateSong);
+  .patch(
+    ensureAuthenticated,
+    authorize(["artist"]),
+    songsStorage.uploadSongFiles,
+    songsStorage.processSongImg,
+    songsStorage.processSongFile,
+    songController.updateSong,
+  );
 
 router.route("/:id/like").post(ensureAuthenticated, songController.like);
 router.route("/:id/dislike").post(ensureAuthenticated, songController.dislike);
