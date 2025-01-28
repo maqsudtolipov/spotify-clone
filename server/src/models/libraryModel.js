27fff917216bc9bda2772343ba11b993ce545a48
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-01-28 20:42:28 +0500

    🚀 Update Library model

--- server/src/models/libraryModel.js
+++ server/src/models/libraryModel.js
@@ -4,15 +4,22 @@ const librarySchema = new mongoose.Schema(
   {
     items: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        pathRef: "docModel",
+        refId: {
+          type: mongoose.Schema.Types.ObjectId,
+          pathRef: "itemType",
+          required: [true, "Please provide a refId"],
+        },
+        itemType: {
+          type: String,
+          required: [true, "Item type is required: Playlist or User"],
+          enum: ["Playlist", "User"],
+        },
+        isPinned: {
+          type: Boolean,
+          default: false,
+        },
       },
     ],
-    docModel: {
-      type: String,
-      required: [true, "Item type is required"],
-      enum: ["Playlist", "User"],
-    },
   },
   {
     toJSON: { virtuals: true },
