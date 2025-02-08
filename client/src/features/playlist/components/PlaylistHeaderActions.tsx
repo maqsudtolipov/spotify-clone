e16aab58f08579807b6209bdeae1fdadcb0c7e65
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-02-08 14:08:30 +0500

    🚀 Feature: Add the delete playlist button and navigate user home when clicked

--- client/src/features/playlist/components/PlaylistHeaderActions.tsx
+++ client/src/features/playlist/components/PlaylistHeaderActions.tsx
@@ -4,12 +4,23 @@ import { RiDeleteBin6Line, RiMoreFill } from 'react-icons/ri';
 import styles from '../../../components/PlayHeader/PlayHeader.module.scss';
 import DropdownList from '../../../ui-library/Dropdown/DropdownList.tsx';
 import DropdownItem from '../../../ui-library/Dropdown/DropdownItem.tsx';
+import { useAppDispatch } from '../../../app/hooks.ts';
+import { deletePlaylist } from '../playlistThunks.ts';
+import { useNavigate } from 'react-router-dom';
 
 interface PlaylistHeaderActionsProps {
   id: string;
 }
 
 const PlaylistHeaderActions = ({ id }: PlaylistHeaderActionsProps) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleDeletePlaylist = async (id: string) => {
+    await dispatch(deletePlaylist({ id }));
+    navigate('/');
+  };
+
   return (
     <Dropdown>
       <DropdownTrigger>
@@ -18,7 +29,11 @@ const PlaylistHeaderActions = ({ id }: PlaylistHeaderActionsProps) => {
 
       <DropdownList position="bottom-right">
         {/*<DropdownItem PreIcon={RiEditLine}>Edit Details</DropdownItem>*/}
-        <DropdownItem PreIcon={RiDeleteBin6Line} underline={false}>
+        <DropdownItem
+          PreIcon={RiDeleteBin6Line}
+          underline={false}
+          onClick={() => handleDeletePlaylist(id)}
+        >
           Delete
         </DropdownItem>
         {/*<DropdownItem PreIcon={RiShareForwardBoxFill}>Share</DropdownItem>*/}
