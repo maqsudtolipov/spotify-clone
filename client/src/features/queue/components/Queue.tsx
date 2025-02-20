394e034ed70f5324ad561015c81ca92e4a82b52d
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-02-20 13:50:02 +0500

    🚀 Feature: Open queue panel when panel is clicked

--- client/src/features/queue/components/Queue.tsx
+++ client/src/features/queue/components/Queue.tsx
@@ -2,17 +2,29 @@ import styles from './Queue.module.scss';
 import QueueHeader from './header/QueueHeader.tsx';
 import { RiArrowLeftWideLine } from 'react-icons/ri';
 import QueueList from './list/QueueList.tsx';
+import { useState } from 'react';
 
 interface QueueProps {
   isQueueOpen: boolean;
 }
 
-const Queue = ({  }: QueueProps) => {
+const Queue = ({}: QueueProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
+  const containerClass = isOpen
+    ? `${styles.container} ${styles.containerOpen}`
+    : `${styles.container} ${styles.containerClosed}`;
+
   return (
-    <div className={styles.container}>
-      <div className={styles.toggleArea}>
-        <RiArrowLeftWideLine />
-      </div>
+    <div className={containerClass}>
+      {!isOpen && (
+        <div className={styles.toggleArea} onClick={() => handleOpen()}>
+          <RiArrowLeftWideLine />
+        </div>
+      )}
       <div className={styles.queue}>
         <QueueHeader />
         <QueueList />
