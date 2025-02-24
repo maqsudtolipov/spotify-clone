0c066240b5051a76340ae02f67fe17e8324034d2
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-02-24 07:56:01 +0500

    🎨 Style: Style player buttons

--- client/src/features/player/PlayerActions.tsx
+++ client/src/features/player/PlayerActions.tsx
@@ -1,5 +1,7 @@
+import styles from './PlayerActions.module.scss';
 import { useAppDispatch } from '../../redux/hooks';
 import { playNext, playPrev } from '../queue/queueSlice.ts';
+import { IoPlayCircle, IoPlaySkipBack, IoPlaySkipForward, IoRepeat, IoShuffle } from 'react-icons/io5';
 
 const PlayerActions = () => {
   const dispatch = useAppDispatch();
@@ -13,11 +15,23 @@ const PlayerActions = () => {
   };
 
   return (
-    <div>
-      <div className="flex gap-4 p-2">
-        <button onClick={handlePlayPrev}>prev</button>
-        <button>play</button>
-        <button onClick={handlePlayNext}>next</button>
+    <div className={styles.actions}>
+      <div className={styles.buttonsContainer}>
+        <IoShuffle className={styles.actionBtn} role="button" />
+        <IoPlaySkipBack
+          className={styles.actionBtn}
+          role="button"
+          onClick={handlePlayPrev}
+        />
+
+        <IoPlayCircle className={styles.playPauseBtn} role="button" />
+
+        <IoPlaySkipForward className={styles.actionBtn} role="button"       onClick={handlePlayNext}/>
+        <IoRepeat
+          className={styles.actionBtn}
+          role="button"
+
+        />
       </div>
       <div>line</div>
     </div>
