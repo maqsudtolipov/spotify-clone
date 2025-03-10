7fa67e5a6df06005458940a3e5216eee1b160d68
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-10 12:30:23 +0500

    🚀 Feature: Main search now search songs, artists, playlists and users

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -1,31 +1,43 @@
 const Song = require("../models/songModel");
+const Playlist = require("../models/playlistModel");
+const User = require("../models/userModel");
 
 // TODO: no pagination here as this route only returns  top 5-10 results. Could be added later
+// TODO: needs to return only necessary data
+
 exports.search = async (req, res, next) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 2;
+    const limit = parseInt(req.query.limit) || 10;
 
     const songs = await Song.find({
       name: { $regex: req.query.name, $options: "i" },
-    })
-      .skip(limit * page)
-      .limit(limit);
+    }).limit(limit);
+    const playlists = await Playlist.find({
+      name: { $regex: req.query.name, $options: "i" },
+    });
+    const artists = await User.find({
+      name: { $regex: req.query.name, $options: "i" },
+      role: "artist",
+    });
+    const users = await User.find({
+      name: { $regex: req.query.name, $options: "i" },
+      role: "user",
+    });
 
     res.status(200).json({
       status: "success",
       results: {
         songs,
-        playlists: [],
-        artists: [],
-        profiles: [],
+        playlists,
+        artists,
+        profiles: users,
       },
-      pagination: {
-        total: 400,
-        page: 2,
-        limit: 20,
-        totalPages: 20,
-      },
+      // pagination: {
+      //   total: 400,
+      //   page: 2,
+      //   limit: 20,
+      //   totalPages: 20,
+      // },
     });
   } catch (e) {
     next(e);
