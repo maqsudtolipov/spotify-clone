332c95ba787854f1c98b5c379283838e26eb8037
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-10 12:52:24 +0500

    🚀 Feature: Return only necessary data for main search

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -9,20 +9,43 @@ exports.search = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 10;
 
+    // Needed data: id, name, img
+    // song: artist name, duration
+    // users: role
+    // playlist: owner name
+
     const songs = await Song.find({
       name: { $regex: req.query.name, $options: "i" },
-    }).limit(limit);
+    })
+      .limit(limit)
+      .select("name duration song img")
+      .populate({ path: "song img", select: "url" });
+
     const playlists = await Playlist.find({
       name: { $regex: req.query.name, $options: "i" },
-    });
+    })
+      .limit(limit)
+      .select("name img user")
+      .populate([
+        { path: "img", select: "url" },
+        { path: "user", select: "name" },
+      ]);
+
     const artists = await User.find({
       name: { $regex: req.query.name, $options: "i" },
       role: "artist",
-    });
+    })
+      .limit(limit)
+      .select("name img")
+      .populate({ path: "img", select: "url" });
+
     const users = await User.find({
       name: { $regex: req.query.name, $options: "i" },
       role: "user",
-    });
+    })
+      .limit(limit)
+      .select("name img")
+      .populate({ path: "img", select: "url" });
 
     res.status(200).json({
       status: "success",
