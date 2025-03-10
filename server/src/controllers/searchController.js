499f516f59d2a3552e899f96bbf373003b3d0dc5
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-10 12:56:22 +0500

    🚀 Feature: Refactor main search to search service

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -1,6 +1,4 @@
-const Song = require("../models/songModel");
-const Playlist = require("../models/playlistModel");
-const User = require("../models/userModel");
+const searchService = require("../services/searchService");
 
 // TODO: no pagination here as this route only returns  top 5-10 results. Could be added later
 // TODO: needs to return only necessary data
@@ -9,43 +7,10 @@ exports.search = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 10;
 
-    // Needed data: id, name, img
-    // song: artist name, duration
-    // users: role
-    // playlist: owner name
-
-    const songs = await Song.find({
-      name: { $regex: req.query.name, $options: "i" },
-    })
-      .limit(limit)
-      .select("name duration song img")
-      .populate({ path: "song img", select: "url" });
-
-    const playlists = await Playlist.find({
-      name: { $regex: req.query.name, $options: "i" },
-    })
-      .limit(limit)
-      .select("name img user")
-      .populate([
-        { path: "img", select: "url" },
-        { path: "user", select: "name" },
-      ]);
-
-    const artists = await User.find({
-      name: { $regex: req.query.name, $options: "i" },
-      role: "artist",
-    })
-      .limit(limit)
-      .select("name img")
-      .populate({ path: "img", select: "url" });
-
-    const users = await User.find({
-      name: { $regex: req.query.name, $options: "i" },
-      role: "user",
-    })
-      .limit(limit)
-      .select("name img")
-      .populate({ path: "img", select: "url" });
+    const { songs, playlists, artists, users } = await searchService.searchAllModels(
+      req.query.name,
+      limit,
+    );
 
     res.status(200).json({
       status: "success",
@@ -53,7 +18,7 @@ exports.search = async (req, res, next) => {
         songs,
         playlists,
         artists,
-        profiles: users,
+        users,
       },
       // pagination: {
       //   total: 400,
