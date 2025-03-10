2eb241d81f58db9e027f05bf9c899c56a17ac7f1
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-10 13:45:15 +0500

    🚀 Feature: Create search types file

--- client/src/features/search/searchSlice.ts
+++ client/src/features/search/searchSlice.ts
@@ -1,8 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { SearchState } from './searchTypes.ts';
+
+const initialState: SearchState = {
+  query: '',
+};
 
 const searchSlice = createSlice({
   name: 'search',
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
