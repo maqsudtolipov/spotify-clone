8d0c71b61532c5b6ba8a4e28af1b93996c40815b
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-11 09:27:18 +0500

    🚀 Feature: Add songs search with pagination

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -1,4 +1,5 @@
 const searchService = require("../services/searchService");
+const Song = require("../models/songModel");
 
 // TODO: no pagination here as this route only returns  top 5-10 results. Could be added later
 // TODO: needs to return only necessary data
@@ -26,18 +27,25 @@ exports.search = async (req, res, next) => {
 
 exports.searchSongs = async (req, res, next) => {
   try {
-    const limit = parseInt(req.query.limit) || 10;
-    const page = parseInt(req.query.page) || 1;
+    const limit = Math.max(parseInt(req.query.limit) || 10);
+    const page = Math.max(parseInt(req.query.page) || 1);
+
+    const filter = { name: { $regex: req.query.name, $options: "i" } };
+
+    const totalCount = await Song.countDocuments(filter); // Count of matching documents
+    const totalPages = Math.ceil(totalCount / limit);
+    const validPage = Math.min(page, totalPages || 1); // Prevent over-paging
+
+    const songs = await Song.find(filter)
+      .limit(limit)
+      .skip((validPage - 1) * limit);
 
     res.status(200).json({
       status: "success",
-      songs: [],
-      pagination: {
-        total: 400,
-        page: 1,
-        limit: 10,
-        totalPages: 40,
-      },
+      songs,
+      currentPage: validPage,
+      totalPages,
+      totalCount,
     });
   } catch (e) {
     next(e);
