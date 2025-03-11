98457b9022c729f581ca4aa581b2cf85d32d44ac
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-11 09:37:34 +0500

    🚀 Feature: Create re-usable function for search pagination

--- server/src/controllers/searchController.js
+++ server/src/controllers/searchController.js
@@ -1,5 +1,6 @@
 const searchService = require("../services/searchService");
 const Song = require("../models/songModel");
+const { getPaginationResults } = require("../helpers/searchHelpers");
 
 // TODO: no pagination here as this route only returns  top 5-10 results. Could be added later
 // TODO: needs to return only necessary data
@@ -54,14 +55,10 @@ exports.searchSongs = async (req, res, next) => {
 
 exports.searchSongs = async (req, res, next) => {
   try {
-    const limit = Math.max(parseInt(req.query.limit) || 10);
-    const page = Math.max(parseInt(req.query.page) || 1);
-
     const filter = { name: { $regex: req.query.name, $options: "i" } };
 
-    const totalCount = await Song.countDocuments(filter); // Count of matching documents
-    const totalPages = Math.ceil(totalCount / limit);
-    const validPage = Math.min(page, totalPages || 1); // Prevent over-paging
+    const { limit, totalCount, totalPages, validPage } =
+      await getPaginationResults(Song, filter, req.query.limit, req.query.page);
 
     const songs = await Song.find(filter)
       .limit(limit)
@@ -70,9 +67,12 @@ exports.searchSongs = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       songs,
-      currentPage: validPage,
-      totalPages,
-      totalCount,
+      pagination: {
+        limit,
+        currentPage: validPage,
+        totalPages,
+        totalCount,
+      },
     });
   } catch (e) {
     next(e);
