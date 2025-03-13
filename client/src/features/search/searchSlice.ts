9c161a73e9bc61b39f1c8ebaff84c06ea4d33166
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-13 21:19:00 +0500

    🐛 Fix: Remove old songs when query changes

--- client/src/features/search/searchSlice.ts
+++ client/src/features/search/searchSlice.ts
@@ -26,16 +26,12 @@ const searchSlice = createSlice({
         state.mainSearch.lastQuery = state.query;
       })
       .addCase(searchSongs.fulfilled, (state, action) => {
+        if (state.songs.lastQuery === state.query)
+          state.songs.items.push(...action.payload.songs);
+        else state.songs.items = action.payload.songs;
+
         state.songs.lastQuery = state.query;
         state.songs.pagination = action.payload.pagination;
-        state.songs.items.push(...action.payload.songs);
-
-        /* - Pagination
-         * Initially page 1 is loaded
-         * if: (page 1 and has items) && (not last page)
-         * fetch next page and update pagination
-         * else: ignore
-         */
       })
       .addCase(searchPlaylists.fulfilled, (state, action) => {
         state.playlists.items = action.payload.playlists;
