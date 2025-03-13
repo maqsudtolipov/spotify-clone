3c0f1d34084436f0e7e0be177a14c5ef8964587d
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-13 20:21:13 +0500

    🟦 Type: Add type coverage for search feature

--- client/src/features/search/searchTypes.ts
+++ client/src/features/search/searchTypes.ts
@@ -2,15 +2,31 @@ import { Artist, Song } from '../artist/artistTypes.ts';
 import { Playlist } from '../playlist/playlistTypes.ts';
 import { User } from '../user/userTypes.ts';
 
+interface Pagination {
+  limit: number;
+  currentPage: number;
+  totalPages: number;
+  totalCount: number;
+}
+
+interface SearchTab<T> {
+  items: T[];
+  pagination: Pagination;
+  lastQuery: string;
+}
+
 export interface SearchState {
   tab: 'all' | 'artists' | 'playlists' | 'songs' | 'profiles';
   query: string;
   mainSearch: {
     songs: Song[];
+    artists: Artist[];
+    playlists: Playlist[];
+    users: User[];
     lastQuery: string;
   };
-  songs: { songs: Song[]; lastQuery: string };
-  playlists: { playlists: Playlist[]; lastQuery: string };
-  artists: { artists: Artist[]; lastQuery: string };
-  users: { users: User[]; lastQuery: string };
+  songs: SearchTab<Song>;
+  playlists: SearchTab<Playlist>;
+  artists: SearchTab<Artist>;
+  users: SearchTab<User>;
 }
