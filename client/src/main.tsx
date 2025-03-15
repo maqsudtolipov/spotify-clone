751190ebcb928c1e30a9c6f17add7a277eee5b06
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-15 10:12:48 +0500

    🚀 Feature: Convert main file to TS

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import AppRoutes from './AppRoutes.jsx';
+import AppRoutes from './AppRoutes.tsx';
 import './index.scss';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.ts';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root') as HTMLDivElement;
+
+createRoot(root).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -15,5 +17,5 @@ createRoot(document.getElementById('root')).render(
         <Toaster position="top-right" />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
