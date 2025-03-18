629c9371742bc11b95ba2a7842302dbbe96c5306
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-18 12:27:08 +0500

    ♻️ Refactor: Connect Input component to use hook form

--- client/src/features/user/components/Login.tsx
+++ client/src/features/user/components/Login.tsx
@@ -5,6 +5,7 @@ import { RiLoaderFill } from 'react-icons/ri';
 import { Link, Navigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks.ts';
 import { login } from '../userThunks.ts';
+import Input from '../../../ui/Input/Input.tsx';
 
 interface FormInput {
   email: string;
@@ -40,32 +41,22 @@ const Login = () => {
           onSubmit={handleSubmit((data) => handleFormSubmit(data))}
         >
           <p className={styles.title}>Spotify</p>
-          <div className={styles.container}>
-            <input
-              className={`${styles.input} ${errors['email'] ? styles.inputInvalid : ''}`}
-              type="email"
-              placeholder="Enter your email"
-              {...register('email', { required: true })}
-            />
 
-            {errors?.email?.type === 'required' && (
-              <p className={styles.error}>This field is required</p>
-            )}
-          </div>
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            register={register('email', { required: true })}
+            error={errors.email}
+            errorMessages={{ required: 'This field is required' }}
+          />
 
-          <div className={styles.container}>
-            <input
-              className={`${styles.input} ${errors['password'] ? styles.inputInvalid : ''}`}
-              type="password"
-              placeholder="Enter your password"
-              {...register('password', {
-                required: true,
-              })}
-            />
-            {errors?.email?.type === 'required' && (
-              <p className={styles.error}>This field is required</p>
-            )}
-          </div>
+          <Input
+            type="password"
+            placeholder="Enter your password"
+            register={register('password', { required: true })}
+            error={errors.password}
+            errorMessages={{ required: 'This field is required' }}
+          />
 
           <button className={styles.button} type="submit">
             {status === 'pending' && <RiLoaderFill />}
--- client/src/ui/Input/Input.tsx
+++ client/src/ui/Input/Input.tsx
@@ -1,13 +1,17 @@
 import styles from './Input.module.scss';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 
 interface InputProps {
   type: string;
-  name: string;
+  name?: string;
+  error?: FieldError;
+  register?: UseFormRegisterReturn;
   defaultValue?: string;
   label?: string;
   placeholder?: string;
   isValid?: boolean;
   onChange?: (arg0: any) => any;
+  errorMessages?: Record<string, string>
 }
 
 const Input = ({
@@ -17,10 +21,13 @@ const Input = ({
   label,
   placeholder,
   isValid,
+  error,
+  register,
+  errorMessages,
   onChange,
 }: InputProps) => {
   return (
-    <div>
+    <div className={styles.container}>
       {label && (
         <label className={styles.label} htmlFor="">
           {label}
@@ -28,13 +35,20 @@ const Input = ({
       )}
       <input
         id={name}
-        className={`${styles.input} ${type === 'file' ? styles.inputFile : ''} ${isValid ? '' : styles.inputInvalid}`}
+        className={`${styles.input} ${type === 'file' ? styles.inputFile : ''} ${isValid ? '' : styles.inputInvalid} ${error ? styles.inputInvalid : ''}`}
         type={type}
         name={name}
         placeholder={placeholder}
         onChange={onChange}
         defaultValue={defaultValue}
+        {...register}
       />
+
+      {error && (
+        <p className={styles.error}>
+          {errorMessages?.[error.type] || 'Invalid input'}
+        </p>
+      )}
     </div>
   );
 };
