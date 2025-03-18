bdce3fbd64c652e77b5bbee3c11df5b760f495bc
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-18 21:55:20 +0500

    ♻️ Refactor: Update DB connection method

--- server/src/utils/connectDB.js
+++ server/src/utils/connectDB.js
@@ -2,9 +2,23 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const DB = process.env.DB_URL;
-    await mongoose.connect(DB);
-    console.log(`🟢 DATABASE CONNECTED`);
+    const env = process.env.NODE_ENV || "development";
+    const DB_URL = {
+      production: process.env.MONGO_URI_PROD,
+      development: process.env.MONGO_URI_DEV,
+      test: process.env.MONGO_URI_TEST,
+    }[env];
+
+    if (!DB_URL) {
+      console.log(`🟠 No MongoDB URI found for environment: ${env}`);
+    }
+
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`🟢 DATABASE CONNECTED to ${env} database`);
   } catch (err) {
     console.error("🔴 DATABASE CONNECTION ERROR: ", err);
     process.exit(1);
