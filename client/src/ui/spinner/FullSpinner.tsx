81c0ceaff5133157058d24855f2c3f9c482e9135
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-18 13:24:10 +0500

    🎨 Style: Change spinner color

--- client/src/ui/spinner/FullSpinner.tsx
+++ client/src/ui/spinner/FullSpinner.tsx
@@ -3,7 +3,7 @@ import spinnerSvg from '../../assets/icons/spinner.svg';
 const FullSpinner = () => {
   return (
     <div className="h-screen w-screen bg-neutral-800 flex items-center justify-center select-none">
-      <img src={spinnerSvg} alt="Loading spinner" className="size-16 opacity-20" />
+      <img src={spinnerSvg} alt="Loading spinner" className="size-16 opacity-80" />
     </div>
   );
 };
