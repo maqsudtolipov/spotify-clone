5bc8af720690cb871dd37e6a548efe08d4020611
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-19 10:16:25 +0500

    🧪 Test: Update authorize.test.js

--- server/test/unit/middlewares/authorize.test.js
+++ server/test/unit/middlewares/authorize.test.js
@@ -1,19 +1,8 @@
 const authorize = require("../../../src/middlewares/authorize");
 const middlewareMock = require("../../helpers/middlewareMock");
 const User = require("../../../src/models/userModel");
-const app = require("../../../src/config/app.config");
-const httpMocks = require("node-mocks-http");
 
 jest.mock("../../../src/models/userModel");
-
-let server;
-beforeAll(async () => {
-  server = app.listen(3009);
-});
-
-afterAll(async () => {
-  server.close();
-});
 
 beforeEach(() => {
   jest.resetAllMocks();
