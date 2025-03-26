837e8d2d461aa345853aac85120e50efc35f4a26
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-27 01:00:09 +0500

    ♻️ Refactor: Hide unused buttons

--- client/src/layout/Nav/components/Logo.jsx
+++ client/src/layout/Nav/components/Logo.jsx
@@ -1,7 +1,7 @@
 import styles from '../Nav.module.scss';
 
 const Logo = () => {
-  return <div className={styles.logo}>Spotify</div>;
+  return <div className={styles.logo}>Spotify Clone</div>;
 };
 
 export default Logo;
