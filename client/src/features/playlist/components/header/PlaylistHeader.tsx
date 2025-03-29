7c20f3967c64a90f400207891f9d7b5782899faf
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-03-29 14:29:56 +0500

    ♻️ Refactor: Separate playlist ImageHeader to different component

---
+++ client/src/features/playlist/components/header/PlaylistHeader.tsx
@@ -0,0 +1,38 @@
+import { Playlist } from '../../playlistTypes.ts';
+import secondsToHourAndMins from '../../helpers/secondsToHourAndMins.ts';
+import styles from '../../../../ui/ImageHeader/ImageHeader.module.scss';
+
+interface ImageHeaderProps {
+  data: Playlist;
+  color: string;
+  textColor: string;
+}
+
+const PlaylistHeader = ({ data, color, textColor }: ImageHeaderProps) => {
+  const gradient = {
+    background: `linear-gradient(${color}, ${color}), linear-gradient(#171717, #171717)`,
+  };
+
+  return (
+    <header className={styles.header} style={gradient}>
+      <img className={`${styles.img}`} src={data.img.url} alt={data.name} />
+
+      <div>
+        <span className={styles.type}>Playlist</span>
+        <h1 className={styles.name} style={{ color: textColor }}>
+          {data.name}
+        </h1>
+
+        {data.description && (
+          <p className={styles.description}>{data.description}</p>
+        )}
+
+        <div className={styles.statistics}>
+          {`• ${data.length} songs, ${secondsToHourAndMins(data.duration)}`}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PlaylistHeader;
