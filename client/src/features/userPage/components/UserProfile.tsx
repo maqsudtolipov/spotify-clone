fdb572eccc4c747d5bcc7a814e9fa8b9a743c256
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-04-02 10:43:14 +0500

    ♻️ Refactor: UserProfile component

--- client/src/features/userPage/components/UserProfile.tsx
+++ client/src/features/userPage/components/UserProfile.tsx
@@ -8,20 +8,22 @@ import NotFound from '../../../ui/statusScreens/NotFound.tsx';
 import ServerError from '../../../ui/statusScreens/ServerError.tsx';
 import UserHeader from './header/UserHeader.tsx';
 import useDominantColor from '../../../hooks/useDominantColor.ts';
+import styles from '../../../ui/PlayHeader/PlayHeader.module.scss';
+import TransparentButton from '../../../ui/Button/TransparentButton.tsx';
+import { followUser, unfollowUser } from '../../user/userThunks.ts';
 
 const isFollowed = (id: string, followings: string[]) => {
   return followings.includes(id);
 };
 
 const UserProfile = () => {
-  const { id } = useParams<{ id: string }>();
   const { status, statusCode, error } = useAppSelector(
     (state) => state.userPage.api.getUser,
   );
   const data = useAppSelector((state) => state.userPage?.data);
-  const { followings, id: userId } = useAppSelector(
-    (state) => state.user?.data,
-  );
+  const user = useAppSelector((state) => state.user?.data);
+
+  const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
@@ -31,6 +33,10 @@ const UserProfile = () => {
     if (id) dispatch(getUser({ id }));
   }, [id]);
 
+  if (!user) return null;
+
+  const { followings, id: userId } = user;
+
   if (status === 'rejected') {
     if (statusCode === 404) return <NotFound message={error} />;
     if (statusCode === 500) return <ServerError />;
@@ -38,27 +44,23 @@ const UserProfile = () => {
   }
   if (status === 'pending' || !data || !id) return <LoadingScreen />;
 
-  console.log(data.img.url);
+  const toggleFollow = () =>
+    isFollowed(id, followings)
+      ? dispatch(unfollowUser({ id, type: 'user' }))
+      : dispatch(followUser({ id, type: 'user' }));
 
   return (
     <>
       <UserHeader data={data} bgColor={bgColor} textColor={textColor} />
-      {/*<ImageHeader data={{ ...data, statistics }} type="user" />*/}
       <GradientBackground color={bgColor}>
-        ok
-        {/*{' '}*/}
-        {/*<div className={styles.playerHeader}>*/}
-        {/*  {id !== userId && (*/}
-        {/*    <TransparentButton*/}
-        {/*      text={isFollowed(id, followings) ? 'Unfollow' : 'Follow'}*/}
-        {/*      onClick={() =>*/}
-        {/*        isFollowed(id, followings)*/}
-        {/*          ? dispatch(unfollowUser({ id, type: 'user' }))*/}
-        {/*          : dispatch(followUser({ id, type: 'user' }))*/}
-        {/*      }*/}
-        {/*    ></TransparentButton>*/}
-        {/*  )}*/}
-        {/*</div>*/}
+        <div className={styles.playerHeader}>
+          {id !== userId && (
+            <TransparentButton
+              text={isFollowed(id, followings) ? 'Unfollow' : 'Follow'}
+              onClick={() => toggleFollow()}
+            ></TransparentButton>
+          )}
+        </div>
       </GradientBackground>
     </>
   );
