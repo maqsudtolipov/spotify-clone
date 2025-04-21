737a31b1771b83aeab645fc5d12d298aae47556e
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-04-21 16:10:09 +0500

    ♻️ Refactor: Deployment changes

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -177,7 +177,7 @@ userSchema.statics.getDefaultUserImgId = async function () {
       fileId: "user",
       name: "defaultUser.jpeg",
       size: 0,
-      filePath: "spotify/users/defaultUser.jpeg",
+      filePath: "defaultUser.jpeg",
       url: process.env.DEFAULT_USER_IMR_URL,
       isDefault: true,
     });
