04538308e31fbfa1e50ee9a252fe8c4730cc55d6
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-04-28 12:12:17 +0500

    🚀 Feature: Create play count model

---
+++ server/src/feature/playCount/PlayCount.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const playCountSchema = new mongoose.Schema(
+  {
+    totalPlays: {
+      type: Number,
+    },
+    dailyPlays: [
+      {
+        date: Date,
+        count: Number,
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const PlayCount = new mongoose.model("PlayCount", playCountSchema);
+
+export default PlayCount;
