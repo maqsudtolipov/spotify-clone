ae3d2cb84bd9e835a5138b9cc34e12d3aa2dd028
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-05-20 15:57:53 +0500

    ♻️ Refactor: Close route for data migration

--- server/src/feature/playCount/playCountRoutes.js
+++ server/src/feature/playCount/playCountRoutes.js
@@ -3,6 +3,6 @@ const playCountController = require("./PlayCountController");
 
 const router = express.Router();
 
-router.route("/migrate").get(playCountController.migrateCounts);
+// router.route("/migrate").post(playCountController.migrateCounts);
 
 module.exports = router;
