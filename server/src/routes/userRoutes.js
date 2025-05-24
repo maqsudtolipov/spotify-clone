8967b6b9b72cff45d131fce74fe4d9b034c34a4b
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-05-24 14:56:59 +0500

    🚀 Feature: Add input validation for user routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,7 +2,14 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const authorize = require("../middlewares/authorize");
-const {uploadUserImg, resizeUserImg} = require("../storage/userStorage");
+const { uploadUserImg, resizeUserImg } = require("../storage/userStorage");
+const validatorMiddleware = require("../middlewares/validatorMiddleware");
+const {
+  getUserByIdSchema,
+  updateMeSchema,
+  followUserSchema,
+  unfollowUserSchema,
+} = require("../validations/userValidations");
 
 const router = express.Router();
 
@@ -13,18 +20,35 @@ router.get(
   userController.getAll,
 );
 router.get("/current", ensureAuthenticated, userController.current);
-router.get("/:id", ensureAuthenticated, userController.getUserById);
+
+router.get(
+  "/:id",
+  ensureAuthenticated,
+  validatorMiddleware(getUserByIdSchema, "params"),
+  userController.getUserById,
+);
 
 router.patch(
   "/updateMe",
   ensureAuthenticated,
   uploadUserImg,
   resizeUserImg,
+  validatorMiddleware(updateMeSchema),
   userController.updateMe,
 );
 
 // Follows
-router.post("/follow/:id", ensureAuthenticated, userController.followUser);
-router.post("/unfollow/:id", ensureAuthenticated, userController.unfollowUser);
+router.post(
+  "/follow/:id",
+  ensureAuthenticated,
+  validatorMiddleware(followUserSchema, "params"),
+  userController.followUser,
+);
+router.post(
+  "/unfollow/:id",
+  ensureAuthenticated,
+  validatorMiddleware(unfollowUserSchema),
+  userController.unfollowUser,
+);
 
 module.exports = router;
