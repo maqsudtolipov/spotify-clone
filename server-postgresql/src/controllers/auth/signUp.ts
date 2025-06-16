54180b7df9b7082a708e657dc1c2a55811bb36cf
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 14:53:44 +0500

    🚀 Feature: Crate simple signup route

--- server-postgresql/src/controllers/auth/signUp.ts
+++ server-postgresql/src/controllers/auth/signUp.ts
@@ -1,13 +1,33 @@
 import { Request, Response, NextFunction } from "express";
+import prisma from "./../../config/prisma.config";
 
 export const signUpController = async (
-  req: Request,
+  req: Request<
+    {},
+    {},
+    {
+      name: string;
+      email: string;
+      password: string;
+      passwordConfirm: string;
+    }
+  >,
   res: Response,
   next: NextFunction,
 ) => {
   try {
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      imgId: "test",
+    };
+
+    const user = await prisma.user.create({ data: newUser });
+
     res.status(201).json({
       status: "success",
+      user,
     });
   } catch (e) {
     next(e);
