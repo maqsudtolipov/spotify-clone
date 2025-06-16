4188977ac762fa0b07fa6e94bb9a50af847c6895
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 17:50:28 +0500

    🚀 Feature: Add typescript to global error handler

--- server-postgresql/src/errors/globalErrorHandler.ts
+++ server-postgresql/src/errors/globalErrorHandler.ts
@@ -1,20 +1,59 @@
 import { Request, Response, NextFunction } from "express";
 
-interface ErrorType extends Error {
+interface ResError extends Error {
   statusCode: number;
   status: "fail" | "error";
   isOperational: boolean;
   code?: string;
 }
 
+const productionError = (resError: ResError, res: Response) => {
+  if (resError.isOperational) {
+    res.status(resError.statusCode).json({
+      status: resError.status,
+      message: resError.message,
+      code: resError.code,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
+const developmentError = (
+  resError: ResError,
+  devError: Error,
+  res: Response,
+) => {
+  res.status(resError.statusCode).json({
+    status: resError.status,
+    message: resError.message,
+    code: resError.code,
+    dev: {
+      message: devError.message,
+      error: devError,
+      stack: devError.stack,
+    },
+  });
+};
+
 const globalErrorHandler = (
-  err: ErrorType,
+  err: ResError,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
+  const resError = Object.assign(err);
+
+  if (process.env.NODE_ENV === "development") {
+    developmentError(resError, err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    productionError(resError, res);
+  }
 };
 
 export default globalErrorHandler;
