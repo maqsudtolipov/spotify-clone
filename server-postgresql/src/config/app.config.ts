43ea0ff9e86a6c4526d6ae0615e6eb42319ba57c
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 21:32:04 +0500

    🚀 Feature: Install cookie parser

--- server-postgresql/src/config/app.config.ts
+++ server-postgresql/src/config/app.config.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "../routes/authRoutes";
 import globalErrorHandler from "../errors/globalErrorHandler";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
