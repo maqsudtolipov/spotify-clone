880dc943d351832594fc85922c6b432f057fdc58
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 13:26:35 +0500

    🚀 Test server

--- server-postgresql/src/index.ts
+++ server-postgresql/src/index.ts
@@ -2,4 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/api/users', (req, res, next) => {
+    res.json({
+        status: "success",
+    })
+})
+
 app.listen(3000, () => console.log("Server is running on port 3000"));
