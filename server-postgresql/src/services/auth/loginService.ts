39780db11f37e9f9e7e250611d4ff5198685302f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 21:21:08 +0500

    🚀 Feature: Add user and password validation for login

--- server-postgresql/src/services/auth/loginService.ts
+++ server-postgresql/src/services/auth/loginService.ts
@@ -1,8 +1,25 @@
+import prisma from "../../config/prisma.config";
+import { AppError } from "../../errors/AppError";
+import bcrypt from "bcryptjs";
+
 interface LoginService {
   email: string;
   password: string;
 }
 
-export const loginService = async(data: LoginService) => {
+export const loginService = async (data: LoginService) => {
+  // Check if user exists
+  const user = await prisma.user.findUnique({
+    where: { email: data.email },
+    select: {
+      password: true,
+    },
+  });
+  if (!user) throw new AppError("Invalid email or password", 401);
+
+  // Validate passwords
+  const isMatch = await bcrypt.compare(data.password, user.password);
+  if (!isMatch) throw new AppError("Invalid email or password", 401);
+
   return null;
-}
+};
