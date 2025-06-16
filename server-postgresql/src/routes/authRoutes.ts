3e217fc33ce0bd9a403fb854e749e8a033880082
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-06-16 15:44:01 +0500

    🚀 Feature: Add validation to signup route

--- server-postgresql/src/routes/authRoutes.ts
+++ server-postgresql/src/routes/authRoutes.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { signUpController } from "../controllers/auth/signUp";
+import { validate } from "../middlewares/validate";
+import { signUpSchema } from "../validations/authValidations";
 const router = express.Router();
 
-router.route("/signup").post(signUpController);
+router.route("/signup").post(validate(signUpSchema), signUpController);
 
 export default router;
