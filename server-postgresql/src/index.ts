55cbd1ece9f8cfa0b1bac8f9d1196824b49509c5
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-02 17:59:51 +0500

    📁 File: Save commit - review here

--- server-postgresql/src/index.ts
+++ server-postgresql/src/index.ts
@@ -1,3 +1,6 @@
 import app from "./config/app.config";
+import dotenv from "dotenv";
+
+dotenv.config({path: "./../.env"});
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
--- server-postgresql/src/services/auth/cookies.ts
+++ server-postgresql/src/services/auth/cookies.ts
@@ -1,3 +1,34 @@
 import { Response } from "express";
+import jwt from "jsonwebtoken";
 
-export const attachAccessToken = (res: Response, userId: string) => {};
+export const attachAccessToken = (res: Response, userId: string) => {
+  // generate access token
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  const expiresIn = process.env.ACCESS_TOKEN_EXP;
+
+  if (!secret || !expiresIn || !process.env.NODE_ENV) {
+    console.log("Secret not set");
+    return;
+  }
+
+  const accessToken = jwt.sign({ userId }, secret, { expiresIn: 10 });
+
+  // attach cookie
+  const sameSite = process.env.NODE_ENV === "production" ? "lax" : "strict"
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + Number(process.env.ACCESS_TOKEN_EXPIRATION)),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite,
+    domain:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_DOMAIN
+        : "localhost",
+  };
+
+  res.cookie("accessToken", accessToken, cookieOptions);
+
+  return accessToken;
+};
