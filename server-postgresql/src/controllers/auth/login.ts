ba90522ad99cdaad4cb43cb3440f9e2a65a45307
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-25 12:56:23 +0500

    Fix: Access token not working due to invalid env checks

--- server-postgresql/src/controllers/auth/login.ts
+++ server-postgresql/src/controllers/auth/login.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import { loginService } from "../../services/auth/loginService";
 import { attachAccessToken } from "../../services/auth/cookies";
 
--- server-postgresql/src/services/auth/cookies.ts
+++ server-postgresql/src/services/auth/cookies.ts
@@ -5,28 +5,26 @@ export const attachAccessToken = (res: Response, userId: string) => {
   // generate access token
   const secret = process.env.ACCESS_TOKEN_SECRET;
   const expiresIn = process.env.ACCESS_TOKEN_EXP;
+  const env = process.env.NODE_ENV;
+  const clientDomain = process.env.CLIENT_DOMAIN;
 
-  if (!secret || !expiresIn || !process.env.NODE_ENV) {
-    console.log("Secret not set");
-    return;
+  if (!secret || !expiresIn || !env || !clientDomain) {
+    throw new Error("Secret tokens are not set in cookies.ts");
   }
 
   const accessToken = jwt.sign({ userId }, secret, { expiresIn: 10 });
 
   // attach cookie
   const sameSite: "lax" | "strict" | "none" =
-    process.env.NODE_ENV === "production" ? "lax" : "strict";
+    env === "production" ? "lax" : "strict";
 
   const cookieOptions = {
-    expires: new Date(Date.now() + Number(process.env.ACCESS_TOKEN_EXPIRATION)),
+    expires: new Date(Date.now() + Number(expiresIn)),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: env === "production",
     path: "/",
     sameSite,
-    domain:
-      process.env.NODE_ENV === "production"
-        ? process.env.CLIENT_DOMAIN
-        : "localhost",
+    domain: env === "production" ? clientDomain : "localhost",
   };
 
   res.cookie("accessToken", accessToken, cookieOptions);
